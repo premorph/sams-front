3a68faec9f50ec3db09b0a57216460c68bab9115
ronald escobar <usomebodyelse@gmail.com>
2023-01-20 14:56:34 -0400

    cash

--- src/app/presentation/core/interfaces/cash.interface.ts
+++ src/app/presentation/core/interfaces/cash.interface.ts
@@ -1,3 +1,16 @@
 export type ICash={
-    
+    id?:number;
+    date?:Date;
+    company?:string;
+    store?:string;
+    beneficiary?:string;
+    concept?:string;
+    period?:string;
+    client?:string;
+    datePay?:Date;
+    receivingbank?:string;
+    receivingaccount?:string;
+    credit?:string;
+    debit?:string;
+    balance?:string;
 }
